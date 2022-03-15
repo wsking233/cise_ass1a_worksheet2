5dfb714e8150a58df592bc401520531b655481ec
wsking233 <2310016713@qq.com>
2022-03-16 01:33:22 +0800

    add nodemon package

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,22 @@ const articles = require("./dummydata/articles");
 
 const server = express();
 
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT||5000;
+
 server.get('/', (req,res)=>{
-    res.send("API is running")
+    res.send("API is running and running")
 });
 
 server.get('/api/articles', (req,res)=>{
     res.json(articles)
 });
 
-server.listen(5000, console.log("server is working and listening PORT 5000"));
+server.get('/api/articles/:id',(req,res)=>{
+    const article = articles.find((n)=> n._id === req.params.id);
+    res.send(article);
+    console.log(req.params);
+})
+
+server.listen(PORT, console.log("server is working and listening on PORT ${PORT}"));
