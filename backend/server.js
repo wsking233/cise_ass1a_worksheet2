0142edc1d9c638678c5ccea7a65b4847db2f74bf
wsking233 <2310016713@qq.com>
2022-03-16 19:33:14 +0800

    ignore change

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const PORT = process.env.PORT||5000;
 
 server.get('/', (req,res)=>{
-    res.send("API is running and running")
+    res.send("API is running")
 });
 
 server.get('/api/articles', (req,res)=>{
@@ -22,4 +22,4 @@ server.get('/api/articles/:id',(req,res)=>{
     console.log(req.params);
 })
 
-server.listen(PORT, console.log("server is working and listening on PORT ${PORT}"));
+server.listen(PORT, console.log('server is working and listening on PORT ${PORT}'));
